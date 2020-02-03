66cd34f218646b1f0a025a260ef14458a00f9426
Manuel Moos <z-man@users.sf.net>
2020-02-03 23:37:33 +0100

    Heavily rework MainTimerSync
    The basic principle stays the same. From the input timesteps,
    we first try to extract an appropriate rhythm of physics steps
    for a sequence of game loop runs.
    
    Previously, that rhythm was both calculated using and applied
    to updates with the configured physics_jitter_fix.
    Now, we calculate the rhythm based on a fixed physics_jitter_fix
    of 0.5 because that gives the most stable result. That rhythm is
    then used as a base for the regular update process using the
    configured jitter fix.
    
    The source location where each of the many clamping steps is done
    is shuffled around to hopefully make them more understandable.

--- main/main_timer_sync.cpp
+++ main/main_timer_sync.cpp
@@ -32,16 +32,8 @@
 
 #ifdef DEBUG_ENABLED
 // enable to get extra diagnostics from here
-// #define SYNC_TIMER_DEBUG_ENABLED
+#define SYNC_TIMER_DEBUG_ENABLED
 #endif
-
-void MainFrameTime::clamp_idle(float min_idle_step, float max_idle_step) {
-	if (idle_step < min_idle_step) {
-		idle_step = min_idle_step;
-	} else if (idle_step > max_idle_step) {
-		idle_step = max_idle_step;
-	}
-}
 
 /////////////////////////////////
 
@@ -52,202 +44,62 @@ float MainTimerSync::get_physics_jitter_fix() {
 	return Engine::get_singleton()->get_physics_jitter_fix();
 }
 
-// gets our best bet for the average number of physics steps per render frame
-// return value: number of frames back this data is consistent
-int MainTimerSync::get_average_physics_steps(float &p_min, float &p_max) {
-	p_min = typical_physics_steps[0];
-	p_max = p_min + 1;
-
-	for (int i = 1; i < CONTROL_STEPS; ++i) {
-		const float typical_lower = typical_physics_steps[i];
-		const float current_min = typical_lower / (i + 1);
-		if (current_min > p_max)
-			return i; // bail out if further restrictions would void the interval
-		else if (current_min > p_min)
-			p_min = current_min;
-		const float current_max = (typical_lower + 1) / (i + 1);
-		if (current_max < p_min)
-			return i;
-		else if (current_max < p_max)
-			p_max = current_max;
-	}
-
-	return CONTROL_STEPS;
-}
-
-// advance physics clock by p_idle_step, return appropriate number of steps to simulate
-MainFrameTime MainTimerSync::advance_core(float p_frame_slice, int p_iterations_per_second, float p_idle_step) {
-	MainFrameTime ret;
-
-	ret.idle_step = p_idle_step;
-
-	// simple determination of number of physics iteration
-	time_accum += ret.idle_step;
-	ret.physics_steps = floor(time_accum * p_iterations_per_second);
-
-	int min_typical_steps = typical_physics_steps[0];
-	int max_typical_steps = min_typical_steps + 1;
-
-	// given the past recorded steps and typical steps to match, calculate bounds for this
-	// step to be typical
-	bool update_typical = false;
-
-	for (int i = 0; i < CONTROL_STEPS - 1; ++i) {
-		int steps_left_to_match_typical = typical_physics_steps[i + 1] - accumulated_physics_steps[i];
-		if (steps_left_to_match_typical > max_typical_steps ||
-				steps_left_to_match_typical + 1 < min_typical_steps) {
-			update_typical = true;
-			break;
-		}
-
-		if (steps_left_to_match_typical > min_typical_steps)
-			min_typical_steps = steps_left_to_match_typical;
-		if (steps_left_to_match_typical + 1 < max_typical_steps)
-			max_typical_steps = steps_left_to_match_typical + 1;
-	}
-
-#ifdef DEBUG_ENABLED
-	if (max_typical_steps < 0) {
-		WARN_PRINT_ONCE("max_typical_steps is negative");
-	}
-	if (min_typical_steps < 0) {
-		WARN_PRINT_ONCE("min_typical_steps is negative");
-	}
-#endif
-
-	// try to keep it consistent with previous iterations
-	if (ret.physics_steps < min_typical_steps) {
-		const int max_possible_steps = floor((time_accum)*p_iterations_per_second + get_physics_jitter_fix());
-		if (max_possible_steps < min_typical_steps) {
-			ret.physics_steps = max_possible_steps;
-			update_typical = true;
-		} else {
-			ret.physics_steps = min_typical_steps;
-		}
-	} else if (ret.physics_steps > max_typical_steps) {
-		const int min_possible_steps = floor((time_accum)*p_iterations_per_second - get_physics_jitter_fix());
-		if (min_possible_steps > max_typical_steps) {
-			ret.physics_steps = min_possible_steps;
-			update_typical = true;
-		} else {
-			ret.physics_steps = max_typical_steps;
-		}
-	}
-
-	if (ret.physics_steps < 0) {
-#ifdef SYNC_TIMER_DEBUG_ENABLED
-		// negative steps can only happen if either the real clock runs backwards (caught there)
-		// or the jitter_fix setting gets changed on the fly.
-		WARN_PRINT_ONCE("negative physics step calculated");
-#endif
-		ret.physics_steps = 0;
-	}
-
-	time_accum -= ret.physics_steps * p_frame_slice;
-
-	// keep track of accumulated step counts
-	for (int i = CONTROL_STEPS - 2; i >= 0; --i) {
-		accumulated_physics_steps[i + 1] = accumulated_physics_steps[i] + ret.physics_steps;
-	}
-	accumulated_physics_steps[0] = ret.physics_steps;
-
-	if (update_typical) {
-		for (int i = CONTROL_STEPS - 1; i >= 0; --i) {
-			if (typical_physics_steps[i] > accumulated_physics_steps[i]) {
-				typical_physics_steps[i] = accumulated_physics_steps[i];
-			} else if (typical_physics_steps[i] < accumulated_physics_steps[i] - 1) {
-				typical_physics_steps[i] = accumulated_physics_steps[i] - 1;
-			}
-		}
-	}
-
-	return ret;
-}
-
 // calls advance_core, keeps track of deficit it adds to animaption_step, make sure the deficit sum stays close to zero
-MainFrameTime MainTimerSync::advance_checked(float p_frame_slice, int p_iterations_per_second, float p_idle_step) {
-	if (p_idle_step <= 0) {
+MainFrameTime MainTimerSync::advance_checked(float p_physics_delta, int p_physics_iterations_per_second, float p_delta) {
+	if (p_delta <= 0) {
 		WARN_PRINT_ONCE("p_idle_step not positive");
 	}
 
 	const float jitter_fix = get_physics_jitter_fix();
 
 	if (fixed_fps != -1)
-		p_idle_step = 1.0 / fixed_fps;
+		p_delta = 1.0f / fixed_fps;
 	else if (jitter_fix > 0)
-		p_idle_step = spike_filter.filter(p_idle_step);
+		p_delta = spike_filter.filter(p_delta);
 
-	const float min_output_step = p_idle_step > 0 ? p_idle_step * .25 : 1E-6;
+	// the canonical stepper always gets updated with jtter_fix of 0.5; that is the maximal value that
+	// won't ever lead to bouncing from border to border, and we want the maximal value possible because
+	// that makes it most likely to find a stable rhythm.
+	canonical_stepper.advance(p_delta, p_physics_delta, p_physics_iterations_per_second, 0.5f, rhythm);
+
+	// update the rhythm from it
+	rhythm.update(canonical_stepper);
+
+	const float min_output_delta = p_delta > 0 ? p_delta * .25f : 1E-6f;
 
 	// compensate for last deficit
-	p_idle_step += time_deficit;
+	p_delta += time_deficit;
 
-	MainFrameTime ret = advance_core(p_frame_slice, p_iterations_per_second, p_idle_step);
-
-	// we will do some clamping on ret.idle_step and need to sync those changes to time_accum,
-	// that's easiest if we just remember their fixed difference now
-	const double idle_minus_accum = ret.idle_step - time_accum;
+	// update the main stepper with the proper configured jitter_fix
+	PlannedStep step = stepper.plan_step(p_delta, p_physics_delta, p_physics_iterations_per_second, get_physics_jitter_fix(), rhythm);
 
 	// first, least important clamping: keep ret.idle_step consistent with typical_physics_steps.
 	// this smoothes out the idle steps and culls small but quick variations.
 	{
 		float min_average_physics_steps, max_average_physics_steps;
-		int consistent_steps = get_average_physics_steps(min_average_physics_steps, max_average_physics_steps);
+		int consistent_steps = rhythm.get_average_physics_steps(min_average_physics_steps, max_average_physics_steps);
 		if (consistent_steps > 3) {
-			ret.clamp_idle(min_average_physics_steps * p_frame_slice, max_average_physics_steps * p_frame_slice);
+			step.clamp_delta(min_average_physics_steps * p_physics_delta, max_average_physics_steps * p_physics_delta);
 		}
 	}
 
 	// second clamping: keep abs(time_deficit) < jitter_fix * frame_slise
-	float max_clock_deviation = jitter_fix * p_frame_slice;
-	ret.clamp_idle(p_idle_step - max_clock_deviation, p_idle_step + max_clock_deviation);
+	float max_clock_deviation = jitter_fix * p_physics_delta;
+	step.clamp_delta(p_delta - max_clock_deviation, p_delta + max_clock_deviation);
 
-	// last clamping: make sure time_accum is between 0 and p_frame_slice for consistency between physics and idle
-	ret.clamp_idle(idle_minus_accum, idle_minus_accum + p_frame_slice);
+	// apply the planned step (this performs the last clamping to keep time_accum in bounds)
+	stepper.execute_step(step, p_physics_delta, min_output_delta);
 
-	// all the operations above may have turned ret.idle_step negative or zero, keep a minimal value
-	if (ret.idle_step < min_output_step) {
-#ifdef SYNC_TIMER_DEBUG_ENABLED
-		WARN_PRINT_ONCE("negative animation timestep calculated");
-#endif
-		ret.idle_step = min_output_step;
-	}
-
-	// restore time_accum
-	time_accum = ret.idle_step - idle_minus_accum;
-
-	// forcing ret.idle_step to be positive may trigger a violation of the
-	// promise that time_accum is between 0 and p_frame_slice
-#ifdef DEBUG_ENABLED
-	if (time_accum < -1E-7) {
-		WARN_PRINT_ONCE("time_accum negative");
-	}
-#endif
-
-	if (time_accum > p_frame_slice) {
-#ifdef SYNC_TIMER_DEBUG_ENABLED
-		WARN_PRINT_ONCE("extra physics steps required to avoid negative timesteps");
-#endif
-		const int extra_physics_steps = floor(time_accum * p_iterations_per_second);
-		time_accum -= extra_physics_steps * p_frame_slice;
-		ret.physics_steps += extra_physics_steps;
-	}
-
-#ifdef DEBUG_ENABLED
-	if (time_accum < -1E-7) {
-		WARN_PRINT_ONCE("time_accum negative");
-	}
-	if (time_accum > p_frame_slice + 1E-7) {
-		WARN_PRINT_ONCE("time_accum larger than p_frame_slice");
-	}
-#endif
-
-	// track deficit
-	time_deficit = p_idle_step - ret.idle_step;
+	MainFrameTime ret;
+	ret.idle_step = step.delta;
+	ret.physics_steps = step.physics_steps;
 
 	// p_frame_slice is 1.0 / iterations_per_sec
 	// i.e. the time in seconds taken by a physics tick
-	ret.interpolation_fraction = time_accum / p_frame_slice;
+	ret.interpolation_fraction = stepper.time_accum * p_physics_iterations_per_second;
+
+	// track deficit
+	time_deficit = p_delta - ret.idle_step;
 
 	return ret;
 }
@@ -257,19 +109,14 @@ float MainTimerSync::get_cpu_idle_step() {
 	uint64_t cpu_ticks_elapsed = current_cpu_ticks_usec - last_cpu_ticks_usec;
 	last_cpu_ticks_usec = current_cpu_ticks_usec;
 
-	return cpu_ticks_elapsed / 1000000.0;
+	return cpu_ticks_elapsed / 1000000.0f;
 }
 
 MainTimerSync::MainTimerSync() :
 		last_cpu_ticks_usec(0),
 		current_cpu_ticks_usec(0),
-		time_accum(0),
 		time_deficit(0),
 		fixed_fps(0) {
-	for (int i = CONTROL_STEPS - 1; i >= 0; --i) {
-		typical_physics_steps[i] = i;
-		accumulated_physics_steps[i] = i;
-	}
 }
 
 // start the clock
@@ -291,4 +138,179 @@ MainFrameTime MainTimerSync::advance(float p_frame_slice, int p_iterations_per_s
 	float cpu_idle_step = get_cpu_idle_step();
 
 	return advance_checked(p_frame_slice, p_iterations_per_second, cpu_idle_step);
+}
+
+/////////////////////////////////
+
+void MainTimerSync::PlannedStep::clamp_delta(float min_delta, float max_delta) {
+	if (delta < min_delta) {
+		delta = min_delta;
+	} else if (delta > max_delta) {
+		delta = max_delta;
+	}
+}
+
+/////////////////////////////////
+
+MainTimerSync::Stepper::Stepper() {
+	for (int i = CONTROL_STEPS - 1; i >= 0; --i) {
+		accumulated_physics_steps[i] = i;
+	}
+}
+
+MainTimerSync::PlannedStep MainTimerSync::Stepper::plan_step(
+		float p_delta,
+		float p_physics_delta,
+		int p_physics_iterations_per_second,
+		float p_jitter_fix,
+		const MainTimerSync::Rhythm &p_rhythm) const {
+#ifdef DEBUG_ENABLED
+	CRASH_COND(fabsf(p_physics_delta * p_physics_iterations_per_second - 1) > 1E-6f);
+#endif
+
+	PlannedStep ret;
+	ret.delta = p_delta;
+
+	// simple determination of number of physics iteration
+	float next_time_accum = time_accum + p_delta;
+	ret.physics_steps = static_cast<int>(floorf(next_time_accum * p_physics_iterations_per_second));
+
+	int min_typical_steps = p_rhythm[0].typical_physics_steps;
+	int max_typical_steps = min_typical_steps + 1;
+
+	// given the past recorded steps and typical steps to match, calculate bounds for this
+	// step to be typical
+	for (int i = 0; i < CONTROL_STEPS - 1; ++i) {
+		int steps_left_to_match_typical = p_rhythm[i + 1].typical_physics_steps - accumulated_physics_steps[i];
+		if (steps_left_to_match_typical > max_typical_steps ||
+				steps_left_to_match_typical + 1 < min_typical_steps) {
+
+			// inconsistent past, impossible to match. Take what we have and run.
+			return ret;
+		}
+
+		if (steps_left_to_match_typical > min_typical_steps)
+			min_typical_steps = steps_left_to_match_typical;
+		if (steps_left_to_match_typical + 1 < max_typical_steps)
+			max_typical_steps = steps_left_to_match_typical + 1;
+	}
+
+#ifdef DEBUG_ENABLED
+	if (max_typical_steps < 0) {
+		WARN_PRINT_ONCE("max_typical_steps is negative");
+	}
+	if (min_typical_steps < 0) {
+		WARN_PRINT_ONCE("min_typical_steps is negative");
+	}
+#endif
+
+	// try to keep it consistent with previous iterations
+	if (ret.physics_steps < min_typical_steps) {
+		const int max_possible_steps = static_cast<int>(floorf(next_time_accum * p_physics_iterations_per_second + p_jitter_fix));
+		if (max_possible_steps < min_typical_steps) {
+			ret.physics_steps = max_possible_steps;
+		} else {
+			ret.physics_steps = min_typical_steps;
+		}
+	} else if (ret.physics_steps > max_typical_steps) {
+		const int min_possible_steps = static_cast<int>(floorf(next_time_accum * p_physics_iterations_per_second - p_jitter_fix));
+		if (min_possible_steps > max_typical_steps) {
+			ret.physics_steps = min_possible_steps;
+		} else {
+			ret.physics_steps = max_typical_steps;
+		}
+	}
+
+	return ret;
+}
+
+void MainTimerSync::Stepper::execute_step(MainTimerSync::PlannedStep &p_step, float p_physics_delta, float p_min_delta) {
+	if (p_step.physics_steps < 0) {
+#ifdef SYNC_TIMER_DEBUG_ENABLED
+		// negative steps can only happen if either the real clock runs backwards (caught there)
+		// or the jitter_fix setting gets changed on the fly.
+		WARN_PRINT_ONCE("negative physics step calculated");
+#endif
+		p_step.physics_steps = 0;
+	}
+
+	// apply timestep
+	time_accum += p_step.delta - p_step.physics_steps * p_physics_delta;
+
+	// clamp time_accum and p_step.delta consistently with it
+	if (time_accum < 0) {
+		p_step.delta -= time_accum;
+		time_accum = 0;
+	} else if (time_accum > p_physics_delta) {
+		p_step.delta -= time_accum - p_physics_delta;
+		time_accum = p_physics_delta;
+	}
+
+	// all the operations above may have turned ret.idle_step negative or zero, keep a minimal value
+	if (p_step.delta < p_min_delta) {
+#ifdef SYNC_TIMER_DEBUG_ENABLED
+		WARN_PRINT_ONCE("negative animation timestep calculated");
+#endif
+
+		// that needs to kick back into time_accum...
+		time_accum += p_step.delta - p_min_delta;
+		p_step.delta = p_min_delta;
+
+		// and that may require extra physics steps to keep time_accum in bounds, again
+		if (time_accum > p_physics_delta) {
+			int extra_steps = static_cast<int>(floorf(time_accum / p_physics_delta));
+			time_accum -= extra_steps * p_physics_delta;
+			p_step.physics_steps += extra_steps;
+		}
+	}
+
+	// keep track of accumulated step counts
+	for (int i = CONTROL_STEPS - 2; i >= 0; --i) {
+		accumulated_physics_steps[i + 1] = accumulated_physics_steps[i] + p_step.physics_steps;
+	}
+	accumulated_physics_steps[0] = p_step.physics_steps;
+}
+
+/////////////////////////////////
+
+MainTimerSync::Rhythm::Entry::Entry() :
+		typical_physics_steps(0) {
+}
+
+/////////////////////////////////
+
+void MainTimerSync::Rhythm::update(const MainTimerSync::Stepper &p_stepper) {
+	for (int i = CONTROL_STEPS - 1; i >= 0; --i) {
+		Entry &entry = entries[i];
+		int &typical = entry.typical_physics_steps;
+		int actual = p_stepper[i];
+
+		// actual steps taken at any point should be either typical or typical + 1
+		if (actual < typical) {
+			typical = actual;
+		} else if (actual - 1 > typical) {
+			typical = actual - 1;
+		}
+	}
+}
+
+int MainTimerSync::Rhythm::get_average_physics_steps(float &p_min, float &p_max) {
+	p_min = entries[0].typical_physics_steps;
+	p_max = p_min + 1;
+
+	for (int i = 1; i < CONTROL_STEPS; ++i) {
+		const float typical_lower = entries[i].typical_physics_steps;
+		const float current_min = typical_lower / (i + 1);
+		if (current_min > p_max)
+			return i; // bail out if further restrictions would void the interval
+		else if (current_min > p_min)
+			p_min = current_min;
+		const float current_max = (typical_lower + 1) / (i + 1);
+		if (current_max < p_min)
+			return i;
+		else if (current_max < p_max)
+			p_max = current_max;
+	}
+
+	return CONTROL_STEPS;
 }
--- main/main_timer_sync.h
+++ main/main_timer_sync.h
@@ -38,35 +38,105 @@ struct MainFrameTime {
 	float idle_step; // time to advance idles for (argument to process())
 	int physics_steps; // number of times to iterate the physics engine
 	float interpolation_fraction; // fraction through the current physics tick
-
-	void clamp_idle(float min_idle_step, float max_idle_step);
 };
 
 class MainTimerSync {
-	// wall clock time measured on the main thread
-	uint64_t last_cpu_ticks_usec;
-	uint64_t current_cpu_ticks_usec;
-
-	// logical game time since last physics timestep
-	float time_accum;
-
-	// current difference between wall clock time and reported sum of idle_steps
-	float time_deficit;
-
 	// number of frames back for keeping accumulated physics steps roughly constant.
 	// value of 12 chosen because that is what is required to make 144 Hz monitors
 	// behave well with 60 Hz physics updates. The only worse commonly available refresh
 	// would be 85, requiring CONTROL_STEPS = 17.
 	static const int CONTROL_STEPS = 12;
 
-	// sum of physics steps done over the last (i+1) frames
-	int accumulated_physics_steps[CONTROL_STEPS];
+	class Rhythm;
 
-	// typical value for accumulated_physics_steps[i] is either this or this plus one
-	int typical_physics_steps[CONTROL_STEPS];
+	struct PlannedStep {
+		float delta;
+		int physics_steps;
+
+		void clamp_delta(float min_delta, float max_delta);
+	};
+
+	// knows how to advance a fixed physics timestep process to given input frame deltas
+	class Stepper {
+	public:
+		Stepper();
+
+		int operator[](int p_i) const {
+			CRASH_COND(p_i < 0);
+			CRASH_COND(p_i >= CONTROL_STEPS);
+			return accumulated_physics_steps[p_i];
+		}
+
+		// prepares advancement of the clock by p_delta, returns number of physics simulation steps to make
+		// assume a physics timestep length p_physics delta and typical updates stored in p_rhythm
+		PlannedStep plan_step(float p_delta, float p_physics_delta, int p_physics_iterations_per_second, float p_jitter_fix, Rhythm const &p_rhythm) const;
+
+		// executes the planned step, advancing time_accum.
+		// p_step can be modified still; it may need clamping
+		// from the input to keek time_accum in the required range
+		void execute_step(PlannedStep &p_step, float p_physics_delta, float p_min_delta = 0.0f);
+
+		// combines the two
+		float advance(float p_delta, float p_physics_delta, int p_physics_iterations_per_second, float p_jitter_fix, Rhythm const &p_rhythm) {
+			PlannedStep step = plan_step(p_delta, p_physics_delta, p_physics_iterations_per_second, p_jitter_fix, p_rhythm);
+			execute_step(step, p_physics_delta);
+			return step.delta;
+		}
+
+		// logical game time since last physics timestep
+		float time_accum;
+
+	private:
+		// sum of physics steps done over the last (i+1) frames
+		int accumulated_physics_steps[CONTROL_STEPS];
+	};
+
+	// keeps track of typical physics updates
+	class Rhythm {
+	public:
+		struct Entry {
+			// typical value for accumulated_physics_steps[i] is either this or this plus one
+			int typical_physics_steps;
+
+			Entry();
+		};
+
+		// updates the typical steps to the factual steps the stepper has taken
+		void update(Stepper const &p_stepper);
+
+		// gets our best bet for the average number of physics steps per render frame
+		// return value: number of frames back this data is consistent
+		int get_average_physics_steps(float &p_min, float &p_max);
+
+		Entry const &operator[](int p_i) const {
+			CRASH_COND(p_i < 0);
+			CRASH_COND(p_i >= CONTROL_STEPS);
+			return entries[p_i];
+		}
+
+	private:
+		Entry entries[CONTROL_STEPS];
+	};
+
+	// wall clock time measured on the main thread
+	uint64_t last_cpu_ticks_usec;
+	uint64_t current_cpu_ticks_usec;
+
+	// current difference between wall clock time and reported sum of idle_steps
+	float time_deficit;
 
 	int fixed_fps;
 
+	// recorded typical physics steps per frame
+	Rhythm rhythm;
+
+	// physics stepper used to fill the rhythm
+	Stepper canonical_stepper;
+
+	// main physics stepper used to calculate actual steps taken
+	Stepper stepper;
+
+	// eliminates delta spikes before we process them
 	TimerSpikeFilter spike_filter;
 
 protected:
@@ -75,15 +145,8 @@ protected:
 	// the typical values as defined by typical_physics_steps
 	float get_physics_jitter_fix();
 
-	// gets our best bet for the average number of physics steps per render frame
-	// return value: number of frames back this data is consistent
-	int get_average_physics_steps(float &p_min, float &p_max);
-
-	// advance physics clock by p_idle_step, return appropriate number of steps to simulate
-	MainFrameTime advance_core(float p_frame_slice, int p_iterations_per_second, float p_idle_step);
-
 	// calls advance_core, keeps track of deficit it adds to animaption_step, make sure the deficit sum stays close to zero
-	MainFrameTime advance_checked(float p_frame_slice, int p_iterations_per_second, float p_idle_step);
+	MainFrameTime advance_checked(float p_physics_delta, int p_physics_iterations_per_second, float p_delta);
 
 	// determine wall clock step since last iteration
 	float get_cpu_idle_step();
