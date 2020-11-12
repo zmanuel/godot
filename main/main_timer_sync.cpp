f44abd0d1760f75fc8412f13300df06bd153c2bf
Manuel Moos <z-man@users.sf.net>
2020-11-12 19:12:31 +0100

    MAX is a macro, so avoid double evaluation

--- main/main_timer_sync.cpp
+++ main/main_timer_sync.cpp
@@ -168,7 +168,8 @@ MainFrameTime MainTimerSync::advance_checked(float p_frame_slice, int p_iteratio
 		p_idle_step = 1.0 / fixed_fps;
 	}
 
-	const float min_output_step = MAX(p_idle_step / 8, 1E-6);
+	float min_output_step = p_idle_step / 8;
+	min_output_step = MAX(min_output_step, 1E-6);
 
 	// compensate for last deficit
 	p_idle_step += time_deficit;
