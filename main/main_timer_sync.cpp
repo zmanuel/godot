51e2764875b125be685d80901b9fc37b2cc149de
Manuel Moos <manuel@moosnet.de>
2021-03-21 09:50:32 +0100

    Extend debug output some more
    Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>

--- main/main_timer_sync.cpp
+++ main/main_timer_sync.cpp
@@ -110,7 +110,7 @@ MainFrameTime MainTimerSync::advance_core(float p_frame_slice, int p_iterations_
 		WARN_PRINT_ONCE("`max_typical_steps` is negative. This could hint at an engine bug or system timer misconfiguration.");
 	}
 	if (min_typical_steps < 0) {
-		WARN_PRINT_ONCE("min_typical_steps is negative");
+		WARN_PRINT_ONCE("`min_typical_steps` is negative. This could hint at an engine bug or system timer misconfiguration.");
 	}
 #endif
 
